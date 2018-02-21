34d89ffd559d115fdaaa6ebb009fcf4b4f497317
Philippe Champagne <pchampagneqc@hotmail.com>
2018-02-21 14:11:00 -0500

    Ajout du projet prReservation

---
+++ prReservation/formReservation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using prGraphiques;
+
+namespace prReservation
+{
+    public partial class formReservation : formBase
+    {
+        public formReservation()
+        {
+            InitializeComponent();
+        }
+    }
+}
