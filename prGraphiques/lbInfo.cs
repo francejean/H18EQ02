bd19a10b4f63f577e89d4769b5cdf0c4cde79b55
Samuel <fanmc34@gmail.com>
2018-02-26 09:46:03 -0500

    Ajout txtInfo et lbInfo, Création du dataset dsChambre

---
+++ prGraphiques/lbInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prGraphiques
+{
+    public partial class lbInfo : Label
+    {
+        public lbInfo()
+        {
+            InitializeComponent();
+            this.AutoSize = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Location = new System.Drawing.Point(163, 55);
+            this.Name = "label3";
+            this.Size = new System.Drawing.Size(42, 17);
+            this.TabIndex = 6;
+            this.Text = "lbInfo";
+        }
+    }
+}
---
+++ prGraphiques/txtInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prGraphiques
+{
+    public partial class txtInfo : TextBox
+    {
+        public txtInfo()
+        {
+            InitializeComponent();
+            this.Location = new System.Drawing.Point(150, 26);
+            this.Name = "textBox1";
+            this.Size = new System.Drawing.Size(100, 22);
+            this.TabIndex = 5;
+        }
+    }
+}
